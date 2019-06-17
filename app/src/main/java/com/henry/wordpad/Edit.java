c257e80295674d2d0133ef49784ae7c74c419e41
henrygao1998 <2412828684@qq.com>
2019-06-17 15:44:29 +0800

    完成记事本大部分功能

--- app/src/main/java/com/henry/wordpad/Edit.java
+++ app/src/main/java/com/henry/wordpad/Edit.java
@@ -1,21 +1,95 @@
 package com.henry.wordpad;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Edit extends AppCompatActivity {
-
-    Button Btn;
+public class Edit extends Activity {
+    private TextView tv_date;
+    private EditText et_content;
+    private Button btn_ok;
+    private Button btn_cancel;
+    private NotesDB DB;
+    private SQLiteDatabase dbread;
+    public static int ENTER_STATE = 0;
+    public static String last_content;
+    public static int id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
+        // 设置无标题
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_edit);
-        Btn = findViewById(R.id.btn_return_4);
-        Btn.setOnClickListener(new View.OnClickListener() {
+        tv_date = (TextView) findViewById(R.id.tv_date);
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        String dateString = sdf.format(date);
+        tv_date.setText(dateString);
+        et_content = (EditText) findViewById(R.id.et_content);
+        // 设置软键盘自动弹出
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        DB = new NotesDB(this);
+        dbread = DB.getReadableDatabase();
+        Bundle myBundle = this.getIntent().getExtras();
+        last_content = myBundle.getString("info");
+        Log.d("LAST_CONTENT", last_content);
+        et_content.setText(last_content);
+        // 确认按钮的点击事件
+        btn_ok = (Button) findViewById(R.id.btn4);
+        btn_ok.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+                // 获取日志内容
+                String content = et_content.getText().toString();
+                Log.d("LOG1", content);
+                // 获取写日志时间
+                Date date = new Date();
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                String dateNum = sdf.format(date);
+                String sql;
+                String sql_count = "SELECT COUNT(*) FROM note";
+                SQLiteStatement statement = dbread.compileStatement(sql_count);
+                long count = statement.simpleQueryForLong();
+                Log.d("COUNT", count + "");
+                Log.d("ENTER_STATE", ENTER_STATE + "");
+                // 添加一个新的日志
+                if (ENTER_STATE == 0) {
+                    if (!content.equals("")) {
+                        sql = "insert into " + NotesDB.TABLE_NAME_NOTES + " values(" + count + "," + "'" + content + "'" + "," + "'" + dateNum + "')";
+                        Log.d("LOG", sql);
+                        dbread.execSQL(sql);
+                    }
+                }
+                // 查看并修改一个已有的日志
+                else {
+                    Log.d("执行命令", "执行了该函数");
+                    String updatesql = "update note set content='" + content + "' where _id=" + id;
+                    dbread.execSQL(updatesql);
+                    // et_content.setText(last_content);
+                }
+                Intent data = new Intent();
+                setResult(2, data);
+                finish();
+            }
+        });
+        btn_cancel = (Button) findViewById(R.id.btn_return_4);
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
@@ -23,5 +97,15 @@ public class Edit extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+        // if (requestCode == 3 && resultCode == 4) {
+        // last_content=data.getStringExtra("data");
+        // Log.d("LAST_STRAING", last_content+"gvg");
+        // }
     }
 }
--- app/src/main/java/com/henry/wordpad/MainActivity.java
+++ app/src/main/java/com/henry/wordpad/MainActivity.java
@@ -1,22 +1,59 @@
 package com.henry.wordpad;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import static android.widget.AbsListView.OnScrollListener.SCROLL_STATE_FLING;
+import static android.widget.AbsListView.OnScrollListener.SCROLL_STATE_IDLE;
+import static android.widget.NumberPicker.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL;
+
+
+public class MainActivity extends AppCompatActivity implements OnScrollListener, OnItemClickListener, OnItemLongClickListener {
+    private Context mContext;
+    private ListView listview;
+    private SimpleAdapter simp_adapter;
+    private List<Map<String, Object>> dataList;
+    private Button addNote;
+    private TextView tv_content;
+    private NotesDB DB;
+    private SQLiteDatabase dbread;
+
 
     private ImageView mImgMenu;
-    /**导航栏左侧的侧边栏的父容器*/
+    /**
+     * 导航栏左侧的侧边栏的父容器
+     */
     private DrawerLayout mDrawerLayout;
     //导航视图
     private NavigationView mNavigationView;
@@ -28,14 +65,44 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
+
+        tv_content = (TextView) findViewById(R.id.tv_content);
+        listview = (ListView) findViewById(R.id.listview);
+        dataList = new ArrayList<Map<String, Object>>();
+
+        mContext = this;
         initViews();
         initEvents();
         Btn1 = findViewById(R.id.btn1);
         Btn2 = findViewById(R.id.btn2);
-        Btn3 = findViewById(R.id.btn3);
-        Btn1.setOnClickListener(new View.OnClickListener() {
+        addNote = (Button) findViewById(R.id.btn3);
+        addNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Edit.ENTER_STATE = 0;
+                Intent intent = new Intent(mContext, Edit.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("info", "");
+                intent.putExtras(bundle);
+                startActivityForResult(intent, 1);
+            }
+        });
+        DB = new NotesDB(this);
+        dbread = DB.getReadableDatabase();
+        // 清空数据库中表的内容
+        // dbread.execSQL("delete from note");
+        RefreshNotesList();
+        listview.setOnItemClickListener(this);
+        listview.setOnItemLongClickListener(this);
+        listview.setOnScrollListener(this);
+
+
+        Btn1.setOnClickListener(new View.OnClickListener()
+
+        {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
@@ -43,7 +110,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        Btn2.setOnClickListener(new View.OnClickListener() {
+        Btn2.setOnClickListener(new View.OnClickListener()
+
+        {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
@@ -51,16 +120,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        Btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, Edit.class);
-                startActivity(intent);
-            }
-        });
-    }
 
+    }
 
     private void initViews() {
         mImgMenu = findViewById(R.id.img_menu);
@@ -80,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
         mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch(item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.menu_info:
                         /* 新建一个Intent对象 */
                         Intent intent = new Intent();
@@ -91,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
                         /* 关闭当前的Activity */
                         MainActivity.this.finish();
                         break;
-                        //Toast.makeText(MainActivity.this, "个人信息", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(MainActivity.this, "个人信息", Toast.LENGTH_SHORT).show();
 
                     case R.id.menu_pwd:
                         Toast.makeText(MainActivity.this, "修改密码", Toast.LENGTH_SHORT).show();
@@ -105,6 +166,124 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void RefreshNotesList() {
+        int size = dataList.size();
+        if (size > 0) {
+            dataList.removeAll(dataList);
+            simp_adapter.notifyDataSetChanged();
+            listview.setAdapter(simp_adapter);
+        }
+        simp_adapter = new SimpleAdapter(this, getData(), R.layout.acitivity_listview, new String[]{"tv_content", "tv_date"}, new int[]{R.id.tv_content, R.id.tv_date});
+        listview.setAdapter(simp_adapter);
+    }
+
+    private List<Map<String, Object>> getData() {
+        Cursor cursor = dbread.query("note", null, "content!=\"\"", null, null, null, null);
+        while (cursor.moveToNext()) {
+            String name = cursor.getString(cursor.getColumnIndex("content"));
+            String date = cursor.getString(cursor.getColumnIndex("date"));
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("tv_content", name);
+            map.put("tv_date", date);
+            dataList.add(map);
+        }
+        cursor.close();
+        return dataList;
+    }
+
+    @Override
+    public void onScroll(AbsListView arg0, int arg1, int arg2, int arg3) {
+        // TODO Auto-generated method stub
+    }
+
+    // 滑动listview监听事件
+    @Override
+    public void onScrollStateChanged(AbsListView arg0, int arg1) {
+        // TODO Auto-generated method stub
+        switch (arg1) {
+            case SCROLL_STATE_FLING:
+                Log.i("main", "用户在手指离开屏幕之前，由于用力的滑了一下，视图能依靠惯性继续滑动");
+            case SCROLL_STATE_IDLE:
+                Log.i("main", "视图已经停止滑动");
+            case SCROLL_STATE_TOUCH_SCROLL:
+                Log.i("main", "手指没有离开屏幕，试图正在滑动");
+        }
+    }
+
+    // 点击listview中某一项的监听事件
+
+    @Override
+    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+        Edit.ENTER_STATE = 1;
+        // Log.d("arg2", arg2 + "");
+        // TextView
+        // content=(TextView)listview.getChildAt(arg2).findViewById(R.id.tv_content);
+        // String content1=content.toString();
+        String content = listview.getItemAtPosition(arg2) + "";
+        String content1 = content.substring(content.indexOf("=") + 1, content.indexOf(","));
+        Log.d("CONTENT", content1);
+        Cursor c = dbread.query("note", null, "content=" + "'" + content1 + "'", null, null, null, null);
+        while (c.moveToNext()) {
+            String No = c.getString(c.getColumnIndex("_id"));
+            Log.d("TEXT", No);
+            // Intent intent = new Intent(mContext, noteEdit.class);
+            // intent.putExtra("data", text);
+            // setResult(4, intent);
+            // // intent.putExtra("data",text);
+            // startActivityForResult(intent, 3);
+            Intent myIntent = new Intent();
+            Bundle bundle = new Bundle();
+            bundle.putString("info", content1);
+            Edit.id = Integer.parseInt(No);
+            myIntent.putExtras(bundle);
+            myIntent.setClass(MainActivity.this, Edit.class);
+            startActivityForResult(myIntent, 1);
+        }
+    }
+
+    @Override
+    // 接受上一个页面返回的数据，并刷新页面
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == 2) {
+            RefreshNotesList();
+        }
+    }    // 点击listview中某一项长时间的点击事件
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+        final int n = arg2;
+        Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("删除该日志");
+        builder.setMessage("确认删除吗？");
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String content = listview.getItemAtPosition(n) + "";
+                String content1 = content.substring(content.indexOf("=") + 1, content.indexOf(","));
+                Cursor c = dbread.query("note", null, "content=" + "'" + content1 + "'", null, null, null, null);
+                while (c.moveToNext()) {
+                    String id = c.getString(c.getColumnIndex("_id"));
+                    String sql_del = "update note set content='' where _id=" + id;
+                    dbread.execSQL(sql_del);
+                    RefreshNotesList();
+                }
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        builder.create();
+        builder.show();
+        return true;
+    }
 
 
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
 }
