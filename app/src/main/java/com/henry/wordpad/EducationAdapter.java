82d284a4d9ff56d20d5cfcc8587cda83fae967c3
henrygao1998 <2412828684@qq.com>
2019-06-21 22:17:41 +0800

    进一步完善按钮事件

---
+++ app/src/main/java/com/henry/wordpad/EducationAdapter.java
@@ -0,0 +1,33 @@
+package com.henry.wordpad;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class EducationAdapter extends FragmentPagerAdapter {
+
+    private String[] title = new String[]{"Domestic","International"};
+
+    public EducationAdapter(FragmentManager manager){
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if(position==0){
+            return new EducationFirstFragment();
+        }else{
+            return new EducationSecondFragment();
+        }
+    }
+
+
+    @Override
+    public CharSequence getPageTitle(int position){
+        return title[position];
+    }
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
